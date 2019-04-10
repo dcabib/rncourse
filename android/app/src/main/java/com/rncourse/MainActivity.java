104a3cfd6ef39d06aea21ef44b1c8da51c49b897
Daniel Abib <daniel.abib@gmail.com>
2019-04-09 21:11:07 -0300

    first commit

---
+++ android/app/src/main/java/com/rncourse/MainActivity.java
@@ -0,0 +1,47 @@
+package com.rncourse;
+
+import com.facebook.react.ReactActivity;
+import com.reactnativenavigation.controllers.SplashActivity;
+
+import com.imagepicker.permissions.OnImagePickerPermissionsCallback; // <- add this import
+import com.facebook.react.modules.core.PermissionListener; // <- add this import
+ 
+public class MainActivity extends SplashActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    // @Override
+    // protected String getMainComponentName() {
+    //     return "rncourse";
+    // }
+    
+}
+
+
+// public class MainActivity extends YourActivity implements OnImagePickerPermissionsCallback {
+//   private PermissionListener listener; // <- add this attribute
+
+//   // Your methods here
+
+//   // Copy from here
+
+//   @Override
+//   public void setPermissionListener(PermissionListener listener)
+//   {
+//     this.listener = listener;
+//   }
+
+//   @Override
+//   public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
+//   {
+//     if (listener != null)
+//     {
+//       listener.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//     }
+//     super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//   }
+
+//   // To here
+// }
